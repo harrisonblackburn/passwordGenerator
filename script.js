fb8545bbf99a19402dffc372f5fd1aad1cb608bd
harrisonblackburn <blackbhr@yahoo.com>
2021-06-05 17:44:58 -0500

    returning random password

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate").addEventListener("click", writePassword);
+var generateBtn = document.querySelector("#generate");
 
-let number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-let specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "~", "=", "/", "<", ">", "?"];
-
-let lowCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-let upCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -17,5 +11,64 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+
+let charTotal = 0; 
+let lowerChar = false;
+let upperChar = false; 
+let numChar = false;
+let specialChar = false; 
+
+let numberOps = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+let specialCharOps = ["!", "%", "&", ",", "*", "+", "-", ".", "~", "=", "/", "<", ">", "?"];
+
+let lowCaseOps = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+let upCaseOps = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+charTotal = prompt("What is the length of your passowrd?")
+
+while (charTotal < 8 || charTotal > 100 || !charTotal.match(/^[0-9]+$/)) {
+  alert ("Please enter a password between 8 and 100 characters")
+  charTotal = prompt("What is the length of your password (must be between 8 and 100 characters)?");
+
+}
+
+lowerChar = confirm("Would you like to use lower case letters?"); 
+
+upperchar = confirm("Would you like to use upper case letters?"); 
+
+numChar = confirm("Would you like to use numerical characters?");
+
+specialChar = confirm("Would you like to use special characters?");
+
+let passwordChar = ""; 
+
+if (lowerChar) {
+  passwordChar += lowCaseOps
+};
+
+if (upperChar) {
+  passwordChar += upCaseOps
+};
+
+if (numChar) {
+  passwordChar += numberOps
+};
+
+if (specialChar) {
+  passwordChar += specialCharOps
+}
+
+let newPassword = ""; 
+
+for (let i=0; i < charTotal; i++) {
+  let char = Math.floor(Math.random() * passwordChar.length)
+  newPassword += passwordChar[char];
+}
+return newPassword
+
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
